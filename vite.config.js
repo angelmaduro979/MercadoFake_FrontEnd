82a42c18153071f3ce9e9f024bae8b31d7694eab
Angel Maduro <angelmaduro979@gmail.com>
2024-12-21 19:06:21 -0300

    proxy commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://tu-backend.com', // Cambia esto por la URL de tu backend
+        changeOrigin: true,
+        secure: false, // Cambia a true si tu backend usa HTTPS con un certificado válido
+      },
+    },
+  },
 })
